d85d3023e67700686baf82b97a695b50c3b4f34b
KotaTsuboi <k17trpsynth@gmail.com>
2023-03-28 21:45:50 +0900

    最初のテンプレートを追加

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i, n) for (int i = 0; i < (int)(n); i++)
+#define REP2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define REQ(i, n) for (int i = 1; i <= (int)(n); i++)
+#define REQ2(i, s, n) for (int i = (s); i <= (int)(n); i++)
+#define ALL(x) (x).begin(), (x).end()
+
+using ll = long long;
+
+int n, a[100005];
+
+int main() {
+  cin >> n;
+
+  REP(i, n) cin >> a[i];
+
+  return 0;
+}
