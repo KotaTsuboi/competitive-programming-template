10fe137790a4d078493ec3fdea0b09baa2d8b556
KotaTsuboi <k17trpsynth@gmail.com>
2023-04-09 18:38:49 +0900

    CMakeとGoogle Testを追加

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include "add.hpp"
 
 using namespace std;
 
---
+++ test.cpp
@@ -0,0 +1,4 @@
+#include "add.hpp"
+#include <gtest/gtest.h>
+
+TEST(test_suite_name, test_name) { EXPECT_EQ(add(1, 1), 2); }
